e63a9662826830c5d0aee0fd42e3d5d17221827f
Hung Vu <leertet96@gmail.com>
2020-01-07 16:57:41 +0100

    add reply behaviour for comments

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -1,32 +1,61 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { useInput } from "../hooks";
 import { request, requestToken } from "../api";
 
-const CommentForm = ({ postId, setTrigger }) => {
+const CommentForm = ({ postId, setTrigger, isReply, commentId }) => {
   const { value: content, setValue: setContent, bind: bindContent } = useInput("");
 
   const handleSubmit = async e => {
     e.preventDefault();
+    const body = { content, post: postId };
+    if (isReply && commentId) body.replyTo = commentId;
+
     const res = await requestToken();
     const { idToken } = await res.json();
     await request({
       method: "POST",
       path: `/posts/${postId}/comments`,
       token: idToken,
-      body: { content, post: postId }
+      body
     });
 
+    // reset text area
     setContent("");
+    // trigger parent rerender
     setTrigger({});
+    unmount();
   };
+
+  const unmount = () => {
+    if (isReply && commentId) {
+      const container = document.querySelector(`#commentform-${commentId}`);
+      container.setAttribute("data-show", "0");
+      ReactDOM.unmountComponentAtNode(container);
+    }
+  };
+
   return (
-    <form className="ui form segment m-0 box-shadow-none no-border" onSubmit={handleSubmit}>
+    <form className={"ui form segment m-0 box-shadow-none no-border " + (isReply ? "p-0" : "")} onSubmit={handleSubmit}>
       <div className="field">
-        <label htmlFor="content">Write your comment:</label>
-        <textarea {...bindContent} name="content" rows="5" placeholder="Express your thoughts.."></textarea>
-        <button type="submit" className="ui button mt-3">
+        {!isReply ? <label htmlFor="content">Write your comment:</label> : ""}
+        <textarea
+          {...bindContent}
+          name="content"
+          rows="5"
+          placeholder="Express your thoughts.."
+          autoFocus={isReply}
+        ></textarea>
+        <button type="submit" className="ui button mt-3 mini">
           Submit
         </button>
+        {isReply && commentId ? (
+          <button className="ui button red mini" onClick={unmount}>
+            Cancel
+          </button>
+        ) : (
+          ""
+        )}
       </div>
     </form>
   );
--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import CommentForm from "./CommentForm";
 import { request } from "../api";
 import VoteArrows from "./VoteArrows";
 
@@ -15,6 +17,20 @@ const CommentList = ({ postId, trigger, setTrigger }) => {
     };
     fetchPost();
   }, [postId, trigger]);
+
+  const handleReply = e => {
+    const container = document.querySelector(`#commentform-${e.target.getAttribute("data-id")}`);
+    if (container.dataset.show === "1") {
+      container.setAttribute("data-show", "0");
+      return ReactDOM.unmountComponentAtNode(container);
+    }
+
+    container.setAttribute("data-show", "1");
+    ReactDOM.render(
+      <CommentForm postId={postId} setTrigger={setTrigger} isReply commentId={e.target.getAttribute("data-id")} />,
+      container
+    );
+  };
 
   const renderComments = (comments, isReply) => {
     return comments.map(comment => {
@@ -43,6 +59,12 @@ const CommentList = ({ postId, trigger, setTrigger }) => {
               <div className="meta">~ {moment(comment.createdAt).fromNow()}</div>
               <div className="text-wrap">{comment.content}</div>
             </div>
+            <div className="meta">
+              <span className="link pointer" onClick={handleReply} data-id={comment._id}>
+                Reply
+              </span>
+            </div>
+            <div id={`commentform-${comment._id}`} data-show="0"></div>
             {renderComments(comment.replies, true)}
           </div>
         </div>
