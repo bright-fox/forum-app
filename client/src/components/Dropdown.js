146d31f305b3eda0af34ff3d79d59276ba271484
bright-fox <leertet96@gmail.com>
2020-03-25 21:14:13 +0100

    Create Dropdown component

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
+
+const Dropdown = ({ heading, children }) => {
+    console.log()
+    const [dropped, setDropped] = useState(false);
+
+    useEffect(() => {
+        // window listener for the next click event
+        const closeDropdown = () => {
+            setDropped(false);
+            window.removeEventListener("click", closeDropdown);
+        }
+        if (!dropped) return;
+        window.addEventListener("click", closeDropdown);
+
+
+        return function cleanUpListener() {
+            window.removeEventListener("click", closeDropdown);
+        }
+    }, [dropped]);
+
+    return (
+        <div className="dropdown-container">
+            <div className={`dropdown-menu ${!dropped ? "" : " active "}`} onClick={() => setDropped(!dropped)}>
+                {heading}
+                <i className="dropdown icon"></i>
+            </div>
+            <div className={`dropdown-options ${!dropped ? "hidden " : ""}`}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Dropdown;
