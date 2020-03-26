3c4fda4f93326fc9a986aa7c1e87d2834a6c0da5
bright-fox <leertet96@gmail.com>
2020-03-26 17:11:01 +0100

    Style the communities page, add loader and 404 error display

--- client/src/components/pages/CommunitiesPage.js
+++ client/src/components/pages/CommunitiesPage.js
@@ -4,17 +4,20 @@ import CommunitySidebar from "../CommunitySidebar";
 import Pagination from "../Pagination";
 import UserContext from "../../contexts/UserContext";
 import { request } from "../../api";
+import { isEmpty } from "../../utils";
+import ErrorDisplay from "../ErrorDisplay";
+import Loader from "../Loader";
 
 const CommunitiesPage = () => {
     const { state } = useContext(UserContext);
-    const [communities, setCommunities] = useState([]);
+    const [communities, setCommunities] = useState(null);
     const [currPage, setCurrPage] = useState(1);
     const [maxPage, setMaxPage] = useState(1);
 
     useEffect(() => {
         const fetchData = async _ => {
             const res = await request({ method: "GET", path: state.isLoggedIn ? `/users/${state.currUser.id}/communities/page/${currPage}` : `/communities/page/${currPage}` })
-            if (res.status !== 200) return;
+            if (res.status !== 200) return setCommunities([]);
             const data = await res.json()
             setCommunities(data.communities);
             setCurrPage(data.currentPage);
@@ -26,18 +29,14 @@ const CommunitiesPage = () => {
     const renderCommunities = () => {
         return communities.map(community => {
             return (
-                <div key={community._id}>
-                    <div className="block border-hover p-3 black">
-                        <img src={`${process.env.PUBLIC_URL}/assets/avatars/community_default.jpg`} alt="community avatar" className="ui avatar image" />
-                        <Link className="link medium bold" to={`/communities/${community._id}`}>{community.name}</Link>
-                        <div className="meta">~ <Link className="link gray" to={`/users/${community.creator._id}`}>{community.creator.username}</Link> | Members: {community.members}</div>
-                        <div className="description">
-                            {community.description}
-                        </div>
+                <div className="border-hover p-3 black" key={community._id}>
+                    <img src={`${process.env.PUBLIC_URL}/assets/avatars/community_default.jpg`} alt="community avatar" className="ui avatar image" />
+                    <Link className="link medium bold" to={`/communities/${community._id}`}>{community.name}</Link>
+                    <div className="meta">~ <Link className="link gray" to={`/users/${community.creator._id}`}>{community.creator.username}</Link> | Members: {community.members}</div>
+                    <div className="description">
+                        {community.description}
                     </div>
-                    <hr />
                 </div>
-
             )
         })
     }
@@ -47,7 +46,10 @@ const CommunitiesPage = () => {
             <CommunitySidebar />
             <div className="ui segment">
                 <h1>{state.isLoggedIn ? "Your communities" : "Communities"}</h1>
-                {renderCommunities()}
+                <div>
+                    {communities ? (!isEmpty(communities) ? renderCommunities() : <ErrorDisplay />) : <Loader />}
+                </div>
+
                 {maxPage > 1 && <Pagination currPage={currPage} maxPage={maxPage} setCurrPage={setCurrPage} />}
             </div>
         </>
